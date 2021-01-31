36998025067b06717e276e08f1d2acf3b29cef6c
kiore <ricardochicas95@gmail.com>
2021-01-31 15:16:40 -0600

    Config .env file and add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './config/typeorm.config';
+import { config } from './config/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+export const config = () => ({
+  database: {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    autoLoadEntities: true,
+    synchronize: true,
+  },
+});
